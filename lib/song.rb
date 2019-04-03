97f8855985c6201d93265faf44769cd0939dd9e7
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-04-03 18:42:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ class Song
   
   def self.genre_count
     @@genres.each do |genres|
-    @@genre_count[genres] ||= 
+    @@genre_count[genres] ||= 0
     @@genre_count[genres] += 1
     binding.pry
   end
